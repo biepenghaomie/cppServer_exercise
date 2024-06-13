a4c49670924793bcd5ce87fb72ce8843f6647372
haomie <454654939@qq.com>
2024-06-14 00:23:41 +0800

    modify:change to linux version

--- client.cpp
+++ client.cpp
@@ -1,24 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <unistd.h>
-#include<winsock2.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <errproccess.h>
-#pragma comment(lib,"ws2.lib")
 
 const int maxn = 10000;
 int main(){
-    WSADATA wsaData;
-    WORD wVersion = MAKEWORD(2, 2);
-    if(WSAStartup(wVersion, &wsaData)){
-        printf("WSAStartup\n");
-    }
 
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     Err::errCheck(sock == -1, "create sock failed");
-    sockaddr_in sockAddr;
+    struct sockaddr_in sockAddr;
     memset(&sockAddr, 0, sizeof(sockAddr));
 
     sockAddr.sin_family = AF_INET;
-    sockAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+    sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     sockAddr.sin_port = htons(8899);
 
     Err::errCheck(connect(sock, (sockaddr *)&sockAddr, sizeof(sockAddr)) == -1, "connect faied");
@@ -29,5 +24,5 @@ int main(){
         writeSize = write(sock, s, sizeof(s));
         Err::errCheck(writeSize == -1, "write failed");
     }
-    closesocket(sock);
+    close(sock);
 }
--- server.cpp
+++ server.cpp
@@ -1,39 +1,31 @@
 #include <bits/stdc++.h>
 #include <string>
-#include <winsock.h>
-#include <errproccess.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <unistd.h>
-#pragma comment(lib,"ws2.lib")
-// using namespace std;
+#include <errproccess.h>
 
-// #define int long long
 
 const int maxn = 10000;
 
 int main()
 {
-    WSADATA wsaData;
-    WORD wVersion = MAKEWORD(2, 2);
-    if(WSAStartup(wVersion, &wsaData)){
-        printf("WSAStartup\n");
-    }
-    std::string s = "qwe";
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     Err::errCheck(sock == -1, "create sock failed");
     sockaddr_in serverAddr;
     memset(&serverAddr, 0, sizeof(serverAddr));
 
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serverAddr.sin_port = htons(8899);
 
     Err::errCheck(bind(sock, (sockaddr *)&serverAddr, sizeof(serverAddr)) == -1, "bind failed");
 
     Err::errCheck(listen(sock, 5) == -1, "listen failed");
     while(1){
-        SOCKET client;
+        int client;
         sockaddr_in clientAddr;
-        int socklen = sizeof(clientAddr);
+        socklen_t socklen = sizeof(clientAddr);
         client = accept(sock, (sockaddr *)&clientAddr, &socklen);
 
         Err::errCheck(client == -1, "accept failed");
@@ -46,6 +38,6 @@ int main()
         Err::errCheck(readSize == -1, "read failed");
         printf("message from client:%s\n", data);
     }
-    closesocket(sock);
+    close(sock);
     return 0;
 }
